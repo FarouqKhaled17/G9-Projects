de0a868f3041742ffed20f1803483cf57c73f393
Farouk Khaled <80914530+FarouqKhaled17@users.noreply.github.com>
2025-04-28 13:21:50 +0300

    some updates in simple calculator project

--- Semester 2/2-simple-calculator/script.js
+++ Semester 2/2-simple-calculator/script.js
@@ -2,12 +2,25 @@ const display = document.getElementById("display");
 
 function appendToDisplay(input) {
     display.value += input;
+    console.log(display.value);
 }
 
 function clearDisplay() {
     display.value = "";
+    console.log("Display cleared");
 }
 
 function calculate() {
     display.value = eval(display.value);
+    console.log("Calculation result: " + display.value);
+}
+
+//! Function to check if the input is empty to show
+function checkInput() {
+    if (display.value === "") {
+        display.value = "Enter a value";
+        console.log("Input is empty, please enter a value");
+    } else {
+        calculate();
+    }
 }
